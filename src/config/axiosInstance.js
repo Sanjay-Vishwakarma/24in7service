f3363706af3bd3a1a087b6e105aa5f5bbbe6bdbe
Sanjay Vishwakarma <hifi785@gmail.com>
2025-08-20 16:08:05 +0530

    server url changed

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:9090/api/v1";
-// const API_BASE_URL = "https://maidserviceprovider.onrender.com/api/v1";
+// const API_BASE_URL = "http://localhost:9090/api/v1";
+const API_BASE_URL = "https://maidserviceprovider.onrender.com/api/v1";
 
 
 const axiosInstance = axios.create({
